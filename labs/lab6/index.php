85a5f00483becf1240f14be5a0327496ef5bbc49
Ruby Boyle <rboyle@csumb.edu>
2019-03-27 18:56:04 +0000

    lab6 more

--- labs/lab6/index.php
+++ labs/lab6/index.php
@@ -7,6 +7,7 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="lab6Styles.css">
         <script>
             /*global $*/
             $(document).ready(function(){
@@ -49,7 +50,7 @@
                       });
                     },
                     complete: function(data,status) { //optional, used for debugging purposes
-                    alert(status);
+                    //alert(status);
                     }
                     
                 });//ajax
@@ -95,14 +96,6 @@
             
         </script>
 
-        <style>
-        
-            .row  { display:flex; }
-        
-            .col1 { width:250px; }
-
-        </style>           
-        
     </head>
     <body>
 
