febc499f49af87b9beba3b830136d22d0bcc3438
Ruby Boyle <rboyle@csumb.edu>
2019-04-22 20:49:31 +0000

    morePractice

---
+++ labPractice/poll/inc/dbConnection.php
@@ -0,0 +1,24 @@
+<?php
+
+    function getDatabaseConnection($dbName) {
+    
+        $host = "localhost";
+        $username = "root";
+        $password = "";
+        
+        //checks whether the URL contains "herokuapp" (using Heroku)
+        if(strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+           $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+           $host = $url["host"];
+           $dbName = substr($url["path"], 1);
+           $username = $url["user"];
+           $password = $url["pass"];
+        }
+        
+        $dbConn = new PDO("mysql:host=$host;dbname=$dbName", $username, $password);
+        $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+        
+        return $dbConn;
+    
+    }
+?>
---
+++ labPractice/poll/inc/functions.php
@@ -0,0 +1,22 @@
+<?php
+
+include 'dbConnection.php';
+
+function updatePoll() {
+    $db = getConnection();
+    
+    $choice = $_GET['userChoice'];
+    $pollId = $_GET['pollId'];
+    $stmt = $db->prepare("UPDATE poll_response SET $choice = $choice + 1 WHERE pollId = :pollId");
+    $stmt -> execute(array(":pollId=>$pollId"));
+    $stmt = $db->prepare("SELECT * FROM poll_response WHERE pollId =:pollId");
+    $stmt -> execute(array(":pollId=>$pollId"));  
+    $record = $stmt->fetch(PDO::FETCH_ASSOC);
+    
+    echo json_encode($record);
+}
+
+if (isset($_GET['userChoice'])) {
+    updatePoll();
+}
+?>
