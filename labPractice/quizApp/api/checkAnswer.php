e753c8517c99e758110e426e02eacc91df24cd9a
Ruby Boyle <rboyle@csumb.edu>
2019-04-29 19:12:23 +0000

    quizapp

---
+++ labPractice/quizApp/api/checkAnswer.php
@@ -0,0 +1,10 @@
+<?php
+
+//receive email and score from the quiz
+
+//1. Get latest score based on email
+//2. If record found, first display it in JSON format, then update record
+//3. If record not found, insert a new record for that email
+
+
+?>
---
+++ labPractice/quizApp/api/dbConnection.php
@@ -0,0 +1,33 @@
+<?php
+
+
+function getDatabaseConnection($dbname = 'tcp'){
+    
+    $host = 'localhost';//cloud 9
+    //$dbname = 'tcp';
+    $username = 'root';
+    $password = '';
+    
+    //using different database variables in Heroku
+    if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $host = $url["host"];
+        $dbname = substr($url["path"], 1);
+        $username = $url["user"];
+        $password = $url["pass"];
+    } 
+    
+    //creates db connection
+    $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    
+    //display errors when accessing tables
+    $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    return $dbConn;
+    
+}
+
+
+
+
+?>
