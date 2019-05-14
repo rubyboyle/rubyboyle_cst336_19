620c2f27232e2218b9728be65862ce0aa8f9bd09
Ruby Boyle <rboyle@csumb.edu>
2019-05-14 02:59:18 +0000

    l

--- final-scheduler/rubric.php
+++ final-scheduler/rubric.php
@@ -21,37 +21,37 @@
     <tbody>
     <tr>
     <td style="text-align:left">1</td>
-    <td style="text-align:left">You provide a ERD diagram representing the data and its relationships. This may be included in Cloud9 as a picture or from a designer tool</td>
+    <td style="text-align:left; color:green;">You provide a ERD diagram representing the data and its relationships. This may be included in Cloud9 as a picture or from a designer tool</td>
     <td style="text-align:left">10</td>
     </tr>
     <tr>
     <td style="text-align:left">2</td>
-    <td style="text-align:left">Tables in MySQL match the ERD and support the requirements of the application</td>
+    <td style="text-align:left; color:green;">Tables in MySQL match the ERD and support the requirements of the application</td>
     <td style="text-align:left">20</td>
     </tr>
     <tr>
     <td style="text-align:left">3</td>
-    <td style="text-align:left">The list of available appointments is pulled from MySQL using the API endpoint and displayed using the specified page design</td>
+    <td style="text-align:left; color:green;">The list of available appointments is pulled from MySQL using the API endpoint and displayed using the specified page design</td>
     <td style="text-align:left">18</td>
     </tr>
     <tr>
     <td style="text-align:left">4</td>
-    <td style="text-align:left">Available times with dates in the past do not show up in the Dashboard list</td>
+    <td style="text-align:left; color:red;">Available times with dates in the past do not show up in the Dashboard list</td>
     <td style="text-align:left">0</td>
     </tr>
     <tr>
     <td style="text-align:left">5</td>
-    <td style="text-align:left">A user can add an available time slot to the MySQL using the API endpoint and displayed using the specified modal design</td>
+    <td style="text-align:left; color:green;">A user can add an available time slot to the MySQL using the API endpoint and displayed using the specified modal design</td>
     <td style="text-align:left">20</td>
     </tr>
     <tr>
     <td style="text-align:left">6</td>
-    <td style="text-align:left">A user can remove an available time slot from MySQL using the API endpoint</td>
+    <td style="text-align:left; color:green;">A user can remove an available time slot from MySQL using the API endpoint</td>
     <td style="text-align:left">15</td>
     </tr>
     <tr>
     <td style="text-align:left">7</td>
-    <td style="text-align:left">The user confirms the removal using the specified modal design</td>
+    <td style="text-align:left; color:green;">The user confirms the removal using the specified modal design</td>
     <td style="text-align:left">3</td>
     </tr>
     <tr>
@@ -61,42 +61,42 @@
     </tr>
     <tr>
     <td style="text-align:left"></td>
-    <td style="text-align:left">This rubric is properly included AND UPDATED (BONUS)</td>
+    <td style="text-align:left; color:green;">This rubric is properly included AND UPDATED (BONUS)</td>
     <td style="text-align:left">2</td>
     </tr>
     <tr>
     <td style="text-align:left">BD</td>
-    <td style="text-align:left">Login works with a User table and BCrypt</td>
+    <td style="text-align:left; color:green;">Login works with a User table and BCrypt</td>
     <td style="text-align:left">15</td>
     </tr>
     <tr>
     <td style="text-align:left">BD</td>
-    <td style="text-align:left">Add Google Signin for app login</td>
+    <td style="text-align:left; color:red;">Add Google Signin for app login</td>
     <td style="text-align:left">0</td>
     </tr>
     <tr>
     <td style="text-align:left">BD</td>
-    <td style="text-align:left">The app is deployed to Heroku</td>
+    <td style="text-align:left; color:green;">The app is deployed to Heroku</td>
     <td style="text-align:left">15</td>
     </tr>
     <tr>
     <td style="text-align:left">BD</td>
-    <td style="text-align:left">A banner file can be uploaded and displayed</td>
+    <td style="text-align:left; color:red;">A banner file can be uploaded and displayed</td>
     <td style="text-align:left">0</td>
     </tr>
     <tr>
     <td style="text-align:left">BD</td>
-    <td style="text-align:left">The user can add multiple available time slots as specified</td>
+    <td style="text-align:left; color:red;">The user can add multiple available time slots as specified</td>
     <td style="text-align:left">0</td>
     </tr>
     <tr>
     <td style="text-align:left">BD</td>
-    <td style="text-align:left">In a separate page, you show the correct list of available time slots to the user who navigates to the correct invitation URL</td>
+    <td style="text-align:left; color:red;">In a separate page, you show the correct list of available time slots to the user who navigates to the correct invitation URL</td>
     <td style="text-align:left">0</td>
     </tr>
     <tr>
     <td style="text-align:left">BD</td>
-    <td style="text-align:left">You correctly implement booking of the appointement, including all side effects</td>
+    <td style="text-align:left; color:red;">You correctly implement booking of the appointement, including all side effects</td>
     <td style="text-align:left">0</td>
     </tr>
     <tr>
